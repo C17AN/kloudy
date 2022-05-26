1fb6bd13bdf09b3eb53d1cd3c9afd3f541abf3c3
chanmin, kim <kimchanmin1@gmail.com>
2022-05-26 23:46:33 +0900

    feat: implement root page prototype

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,28 @@
 import type { NextPage } from "next";
 import Title from "components/common/Title";
+import Image from "next/image";
 
 const RootPage: NextPage = () => {
   return (
     <div className="h-full flex flex-col">
-      <Title text="전체 리소스 목록" />
-      <div>클러스터 및 네임스페이스를 선택해 주세요.</div>
+      <div className="h-full flex flex-col justify-center items-center">
+        <Image
+          src="/images/hero-image.svg"
+          width="360"
+          height="360"
+        />
+        <h1 className="text-gray-700 text-xl font-extralight mb-8">
+          누구나 조작하는 쿠버네티스, Kloudy
+        </h1>
+        <section className="flex gap-6">
+          <button className="py-2 px-4 rounded-lg bg-sky-100 text-gray-500 shadow">
+            클라우디 알아보기
+          </button>
+          <button className="py-2 px-4 rounded-lg bg-white text-gray-600 shadow">
+            요금제 살펴보기
+          </button>
+        </section>
+      </div>
     </div>
   );
 };
